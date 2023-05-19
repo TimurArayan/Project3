4fd61bf12cf01545a7e88763fe5bd3b3bd44664e
Артем <@DESKTOP-KGHREVN>
2023-05-19 23:42:51 +0300

    kek

--- TestProject3/UnitTest1.cs
+++ TestProject3/UnitTest1.cs
@@ -9,6 +9,14 @@ namespace TestProject3
         [TestMethod]
         public void TestMethod1()
         {
+            DataBase dataBase = new DataBase();
+
+            dataBase.openConnection();
+            string querystring = $"insert into accounts_db (names, surnames, emails, passwords) values ('Анжуманя', 'Бегит', '@.2154765312', '123357521312')";
+            SqlCommand sqlCommand = new SqlCommand(querystring, dataBase.getConnection());
+
+
+            Assert.IsTrue(sqlCommand.ExecuteNonQuery() == 1);
         }
     }
 }
